9a6b4be78ed9b9a0f347df7ee394b1aca5d5de5b
Santiago Benjamín Irigoyen <irigoyensantiago623@gmail.com>
2025-03-26 17:36:32 -0300

    26/3/2025

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -8,3 +8,14 @@ lastModifiedDate.textContent = `Last Modified: ${document.lastModified}`;
 
 document.getElementById('copyrightYear').textContent = new Date().getFullYear();
 document.getElementById('lastModified').textContent = "Last Modified: " + document.lastModified;
+
+// Check if the page visit count exists
+if (localStorage.getItem('pageVisits')) {
+    localStorage.setItem('pageVisits', Number(localStorage.getItem('pageVisits')) + 1);
+  } else {
+    localStorage.setItem('pageVisits', 1);
+  }
+  
+  // Display the visit count
+  document.getElementById('visitCounter').innerText = `Page visits: ${localStorage.getItem('pageVisits')}`;
+  
